dd42e9590bed0267246abecd73d80a532e4054ac
Astrid <me@astr.cc>
2020-05-20 20:41:57 +0200

    Properly implement SSR for pronoun pages

--- README.md
+++ README.md
@@ -11,6 +11,3 @@ defined in `package.json`.
 - To start a Next.js development server, use `npm run dev`.
 - To start a production-ready server, use `npm run build`, followed by `npm run start`.
 - To run the app through ESLint/Prettier and Stylelint, use `npm run lint`.
-
-Due to the way the app uses URL paths, pre-rendering pages doesn't make much sense.
-Still looking for ways to potentially fix this (maybe pre-define a bunch of pages based on our preset pronoun list?).
--- src/pages/[...pronouns].tsx
+++ src/pages/[...pronouns].tsx
@@ -1,3 +1,24 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import FrontPage from "../components/FrontPage";
+import { allPronouns, toTemplate } from "../pronouns";
 
 export default FrontPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: allPronouns.map((ps) => {
+      return {
+        params: {
+          pronouns: toTemplate(ps, { shorten: true }).split("/"),
+        },
+      };
+    }),
+    fallback: true,
+  };
+};
