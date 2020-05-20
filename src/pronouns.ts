0867c013ea9ff0344bf8f848bc5f1ac73eefb70d
Astrid <me@astr.cc>
2020-05-20 22:40:07 +0200

    Fix they pronouns

--- src/pronouns.ts
+++ src/pronouns.ts
@@ -218,7 +218,7 @@ allPronouns.push(
   ...[
     "he/him/his/his/himself",
     "she/her/her/hers/herself",
-    "they/them/their/themselves/plural",
+    "they/them/their/theirs/themselves/plural",
     "fae/faer/faer/faers/faerself",
     "ey/em/eir/eirs/eirself",
     "xe/xem/xyr/xyrs/xemself",
